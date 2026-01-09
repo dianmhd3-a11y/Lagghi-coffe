49a6893ccd453cda55aeb9e341acd2c8d5bfc99e
Dian <dianmhd3@gmail.com>
2026-01-09 19:53:52 +0700

    membuat website lagghi coffe

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-// toggle class active
+// toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
+};
+
+// toggle class active untuk search form
+const searchform = document.querySelector(".search form");
+const searchbox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = () => {
+  searchform.classList.toggle("active");
+  searchbox.focus();
 };
 
 // klik di luar sidebar untuk menghilangkan nav
